62890c854c32e16d48d35b6a43981abe0bdab372
SAICSP <chennasaicsp@gmail.com>
2024-07-28 12:34:05 +0530

    modified file

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
     console.log("Current User:", req.user); // Debugging line
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.nowUser = req.user;
+    
     next();
 });
 
