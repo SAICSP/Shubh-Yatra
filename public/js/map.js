274806faeddd974b97c51742c03dd27dfd7f2a71
SAICSP <chennasaicsp@gmail.com>
2024-07-29 16:14:53 +0530

    modified file

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,17 @@
-mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = '<%= process.env.MAP_TOKEN %>';
+const listing = <%- JSON.stringify(list) %>;
 
 const map = new mapboxgl.Map({
     container: 'map',
-    center: listing.geometry.coordinates, 
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: listing.geometry.coordinates,
     zoom: 1
 });
-            
-const marker = new mapboxgl.Marker({color:'Red'})
+
+const marker = new mapboxgl.Marker({ color: 'Red' })
     .setLngLat(listing.geometry.coordinates)
     .setPopup(
-        new mapboxgl.Popup({offset :25}).setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after Booking</p>`)
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after Booking</p>`)
     )
     .addTo(map);
-
-console.log(coordinates);
